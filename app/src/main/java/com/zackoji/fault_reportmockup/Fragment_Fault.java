6b5f162d482c88d5eab9a00e7e1c9a839b4b9913
shredktp <kornth_ebasicxp@hotmail.com>
2015-07-02 11:53:01 +0700

    Add TabLayout at Fault Report

--- app/src/main/java/com/zackoji/fault_reportmockup/Fragment_Fault.java
+++ app/src/main/java/com/zackoji/fault_reportmockup/Fragment_Fault.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -21,17 +22,31 @@ public class Fragment_Fault extends Fragment {
     Button logout_Butt;
     //String username = "test2";
     TabLayout tabLayout;
+    ViewPager pager_fault;
+    PageAdapter_Tab_Fault adapter_tab_fault;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_fault_report, container, false);
 
+        pager_fault = (ViewPager)rootView.findViewById(R.id.pager_tab_fault);
+        adapter_tab_fault = new PageAdapter_Tab_Fault(getChildFragmentManager());
+        pager_fault.setAdapter(adapter_tab_fault);
+
+
+
         tabLayout = (TabLayout) rootView.findViewById(R.id.tabLayout_fault);
         tabLayout.addTab(tabLayout.newTab().setText("Month 1"));
         tabLayout.addTab(tabLayout.newTab().setText("Month 2"));
         tabLayout.addTab(tabLayout.newTab().setText("Month 3"));
 
+        //tabLayout.getTabAt(0).setIcon(R.drawable.android_logo);
+        tabLayout.getTabAt(1).setText("Month 1");
+        tabLayout.getTabAt(2).setText("Month 1");
+//        tabLayout.getTabAt(3).setText("Month 1");
+
+        tabLayout.setupWithViewPager(pager_fault);
         return rootView;
     }
 
