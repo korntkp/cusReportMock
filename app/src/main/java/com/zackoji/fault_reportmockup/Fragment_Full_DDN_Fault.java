06496edf2d8de044a56055f42fd39335b1e78ca1
shredktp <kornth_ebasicxp@hotmail.com>
2015-07-14 10:32:08 +0700

    Fix Bug at Full Report (Tab.setText)

--- app/src/main/java/com/zackoji/fault_reportmockup/Fragment_Full_DDN_Fault.java
+++ app/src/main/java/com/zackoji/fault_reportmockup/Fragment_Full_DDN_Fault.java
@@ -40,9 +40,9 @@ public class Fragment_Full_DDN_Fault extends Fragment {
         tabLayout.addTab(tabLayout.newTab());
         tabLayout.addTab(tabLayout.newTab());
         tabLayout.addTab(tabLayout.newTab());
-        tabLayout.getTabAt(0).setText(1);
-        tabLayout.getTabAt(1).setText(2);
-        tabLayout.getTabAt(2).setText(3);
+        tabLayout.getTabAt(0).setText("1");
+        tabLayout.getTabAt(1).setText("2");
+        tabLayout.getTabAt(2).setText("3");
         tabLayout.setupWithViewPager(pager_full_ddn_fault);
 
         /**
